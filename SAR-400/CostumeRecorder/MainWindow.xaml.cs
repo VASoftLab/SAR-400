6a3b7d59f19f343626e69bd65284156ae7df4064
Aleksandr Larionenko <larik97@gmail.com>
2019-02-20 12:16:37 +0300

    Подключение к SAR
    В GUI добавлена установка файла конфига и подключение к SAR.

--- SAR-400/CostumeRecorder/MainWindow.xaml.cs
+++ SAR-400/CostumeRecorder/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using CostumeController;
+
 namespace CostumeRecorder
 {
     /// <summary>
@@ -20,9 +22,42 @@ namespace CostumeRecorder
     /// </summary>
     public partial class MainWindow : Window
     {
+        Costume SAR;
+        string pathToConfig = string.Empty;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            SAR = new Costume();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonConfig_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog opf = new Microsoft.Win32.OpenFileDialog();
+
+            Nullable<bool> result = opf.ShowDialog();
+
+            if (result == true)
+            {
+                pathToConfig = opf.FileName;
+            }
+        }
+
+        private void ButtonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(pathToConfig) == false)
+                SAR.Initialize(pathToConfig);
+
+            if (SAR.Initialized)
+                MessageBox.Show("Успешно подключено!", "Подключение", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show("Ошибка при подключении!", "Подключение", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
